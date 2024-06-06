708eaa9cb58193ec1d6b9b8d5ba58e8c802d869f
Leonid Kapitonov <Feacur@gmail.com>
2024-06-07 01:22:17 +0200

    initial

---
+++ code/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include "_project.h" // IWYU pragma: keep
+
+int main(int argc, char * argv[]) {
+	printf("main args:\n");
+	for (int i = 0; i < argc; i++) {
+		printf("- %s\n", argv[i]);
+	}
+	fgetc(stdin);
+	return 0;
+}
+
+// ---- ---- ---- ----
+// internal
+// ---- ---- ---- ----
+
+#if BUILD_TYPE == BUILD_TYPE_WINDOWS
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <stdlib.h>
+int main(int argc, char * argv[]);
+int WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow) {
+	return main(__argc, __argv);
+}
+#endif
